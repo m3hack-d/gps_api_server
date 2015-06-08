9f1ae97e49b87b02a36c98c25437c298f868ea3e
r-wataru <wataru@rubyquitous.co.jp>
2015-06-08 23:24:47 +0900

    slackへpush内容変更

--- app/controllers/api/performances_controller.rb
+++ app/controllers/api/performances_controller.rb
@@ -1,7 +1,7 @@
 class Api::PerformancesController < Api::Base
   def index
     render json: { result: "OK" }
-    Slack.chat_postMessage(text: 'test notification to slack', username: 'w-sato', channel: '#general')
+    Slack.chat_postMessage(text: "この内容が送られています。#{params}", username: 'Push Notification', channel: '#general')
     PerformanceMailer.send_information.deliver_now
   end
 end
